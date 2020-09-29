756461cb1409a90f2658723496ecbc60bfde11fd
NRoy-hub <code0zhem@gmail.com>
2020-09-29 23:27:47 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = 80;
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(PORT, () => {
+  console.log(`listening on ${ PORT }`)
+})
