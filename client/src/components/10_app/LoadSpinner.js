d22e88df4cd534a7dd77b5978d8693c1fbbec3e6
NRoy-hub <code0zhem@gmail.com>
2020-10-12 01:17:59 +0900

    client > LoadSpinner 배경색 제거

--- client/src/components/10_app/LoadSpinner.js
+++ client/src/components/10_app/LoadSpinner.js
@@ -21,7 +21,6 @@ const StyledDiv = styled.div`
   top: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(255, 255, 255, 0.3);
   display: flex;
   justify-content: center;
   align-items: center;
